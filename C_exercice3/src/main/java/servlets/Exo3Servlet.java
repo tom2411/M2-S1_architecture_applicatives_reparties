8f53203f40974505a0c8a4ae09d6b3104b695758
Tom Ribardiere <tom.ribardiere@free.fr>
2021-09-10 18:41:30 +0200

    [ADD] Continuation de l'exo 3

--- C_exercice3/src/main/java/servlets/Exo3Servlet.java
+++ C_exercice3/src/main/java/servlets/Exo3Servlet.java
@@ -1,8 +1,10 @@
 package servlets;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.sound.midi.Soundbank;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -25,17 +27,29 @@ public class Exo3Servlet extends HttpServlet {
         if (this.aDeviner==null){
             String lemot = request.getParameter("lemot");
             setaDeviner(lemot);
+            request.setAttribute("devine", this.devine);
+            request.setAttribute("coupRestant", this.nbEssaisRestants);
             request.getRequestDispatcher("/WEB-INF/essai.jsp").forward(request, response);
         }else {
-            char caractere = request.getParameter("lecaractere").charAt(0);
-            if (this.nbEssaisRestants>0 && !this.devine.equals(this.aDeviner)){
-                System.out.println(caractere);
-                boolean res = test(caractere);
-                System.out.println(res);
-                System.out.println("devine="+this.devine);
-                request.getRequestDispatcher("/WEB-INF/essai.jsp").forward(request, response);
-
+            if (request.getParameter("lecaractere").length()==0){
+                request.setAttribute("devine", this.devine);
+                request.setAttribute("erreur", "Vous êtes obligés de choisir une lettre.");
+            }else{
+                char caractere = request.getParameter("lecaractere").charAt(0);
+                if (this.nbEssaisRestants>0 && !this.devine.equals(this.aDeviner)){
+                    System.out.println(caractere);
+                    boolean res = test(caractere);
+                    System.out.println(res);
+                    request.setAttribute("coupRestant", this.nbEssaisRestants);
+                    System.out.println("devine="+this.devine);
+                    request.setAttribute("devine", this.devine);
+                    if (this.devine.indexOf("_")==-1){
+                        request.setAttribute("coup",this.nbEssaisRestants);
+                        request.setAttribute("fin", true);
+                    }
+                }
             }
+            request.getRequestDispatcher("/WEB-INF/essai.jsp").forward(request, response);
         }
     }
 
@@ -47,21 +61,19 @@ public class Exo3Servlet extends HttpServlet {
     }
 
     private boolean test(char carac){
-        System.out.println("debut");
-        boolean res=false;
-        for (int last=0;last!=-1;last=aDeviner.indexOf(carac,last)) {
-            res = true;
-            System.out.println("last dans le for = "+aDeviner.indexOf(carac,last));
-            last = aDeviner.indexOf(carac,last);
-            devine.setCharAt(last, carac);
-            System.out.println(last);
-            last++;
-        }
-        if (res==false) {
-            System.out.println("nbEssai--");
+        int i=0;
+        if (aDeviner.indexOf(carac, i)==-1){
             nbEssaisRestants--;
+            return false;
         }
-        System.out.println("sortie");
-        return res;
+        while (i!=aDeviner.length()-1){
+            int index_premiere_lettre = aDeviner.indexOf(carac, i);
+            if (index_premiere_lettre==-1){
+                return true;
+            }
+            devine.setCharAt(index_premiere_lettre,carac);
+            i++;
+        }
+        return true;
     }
 }
